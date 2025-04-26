2c100e9b5ce020f692df98c5418ec0d84cce0bea
Justice Kenechukwu Kanayo <justicesmacboookair@Justices-MacBook-Air.local>
2025-04-26 20:27:57 +0100

    data ingestion research and code modularization

---
+++ src/trendline/config/configuration.py
@@ -0,0 +1,35 @@
+from src.trendline.constants import *
+from src.trendline.utils.common import read_yaml, create_directories
+from src.trendline.entity import (DataIngestionConfig)
+
+
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+
+            # data_ingestion_configuration_manager
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
+        )
+
+        return data_ingestion_config
---
+++ src/trendline/utils/common.py
@@ -0,0 +1,140 @@
+import os
+from box import ConfigBox
+from box.exceptions import BoxValueError
+import yaml
+from src.trendline.logging import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from pathlib import Path
+from typing import Any
+import pandas as pd
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded succesfully from: {path}")
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+
+
+@ensure_annotations
+def get_target_worksheet_df(file_path: str, sheet_name: str = "3.4.2 (incl CCL)", skip_rows: int = 6, read_rows: int = 83) -> pd.DataFrame:
+    """
+    Loads the specified worksheet from an Excel file and returns it as a DataFrame.
+    """
+    excel_data = pd.ExcelFile(file_path)
+    if sheet_name not in excel_data.sheet_names:
+        raise ValueError(f"Sheet '{sheet_name}' not found. Available sheets: {excel_data.sheet_names}")
+    
+    df = pd.read_excel(excel_data, sheet_name=sheet_name, skiprows=skip_rows, nrows=read_rows)
+    return df
