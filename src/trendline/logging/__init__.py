49166674ea4e9343030ed85ba5a09c88a629d35e
Justice Kenechukwu Kanayo <justicesmacboookair@Justices-MacBook-Air.local>
2025-04-26 20:16:58 +0100

    logging folder error safety net

--- src/trendline/logging/__init__.py
+++ src/trendline/logging/__init__.py
@@ -1,19 +1,23 @@
 import os
-import sys
 import logging
 
-logging_str = "[%(asctime)s: %(levelname)s:%(module)s: %(message)s]"
+# Setup logging format
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
 log_dir = "src/trendline/logging/logs"
 log_filepath = os.path.join(log_dir, "running_log.log")
-os.makedirs(log_dir, exist_ok = True)
+
+# 💡 Only create directory if it truly doesn't exist
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
     format=logging_str,
     handlers=[
         logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout),
+        logging.StreamHandler()
     ]
 )
+
 logger = logging.getLogger("src")
