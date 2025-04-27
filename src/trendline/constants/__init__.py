a378b4b2250c0b9b51a3fba10b75b309d3e95606
Justice Kenechukwu Kanayo <justicesmacboookair@Justices-MacBook-Air.local>
2025-04-27 19:11:30 +0100

    keeping CI/CD light

--- src/trendline/constants/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-CONFIG_FILE_PATH = Path("/Users/justicesmacboookair/Documents/Data-science/SLR-ENP/config/config.yaml")
-PARAMS_FILE_PATH = Path("/Users/justicesmacboookair/Documents/Data-science/SLR-ENP/params.yaml")
-SCHEMA_FILE_PATH = Path("/Users/justicesmacboookair/Documents/Data-science/SLR-ENP/schema.yaml")
